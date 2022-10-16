81434089791a1742e2c7a8fd3814e0c23d21a686
Shubham Sinha <sinhashubham472@live.com>
2022-10-16 10:47:02 +0530

    moved callback dispatcher

--- lib/m3u8_downloader.dart
+++ lib/m3u8_downloader.dart
@@ -162,3 +162,26 @@ class M3u8Downloader {
     return await _channel.invokeMethod("getSavePath", { "url": url });
   }
 }
+
+void callbackDispatcher() {
+
+  // Initialize state necessary for MethodChannels.
+  WidgetsFlutterBinding.ensureInitialized();
+  const MethodChannel backgroundChannel = MethodChannel('vincent/m3u8_downloader_background', JSONMethodCodec());
+
+  backgroundChannel.setMethodCallHandler((MethodCall call) async {
+    final dynamic args = call.arguments;
+    final CallbackHandle handle = CallbackHandle.fromRawHandle(args[0]);
+
+    final Function? closure = PluginUtilities.getCallbackFromHandle(handle);
+
+    if (closure == null) {
+      print('Fatal: could not find callback');
+      exit(-1);
+    }
+
+    closure(args[1]);
+  });
+
+  backgroundChannel.invokeMethod('didInitializeDispatcher');
+}
